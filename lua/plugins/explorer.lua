cd9fb3d30fb9c0946c58c992ac569ad588b78b70
George Hannah <80538951+Ge0rgeHannah@users.noreply.github.com>
2026-07-09 14:26:34 +0200

    attempting to show hidden files in telescope

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = true, -- show dotfiles
+        ignored = true, -- show .gitignore'd files
+      },
+    },
+  },
+}
